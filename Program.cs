302369734334d4ea51f8ffc85565e6a9602d8516
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-25 13:39:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Part_6___Loops_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+            string choice = "";
+
+            while (!quit)
+            {
+                Console.WriteLine("Welcome back to the most copy-paste menu ever!");
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine();
+                Console.WriteLine("1. Prompting Time");
+                Console.WriteLine("2. Using an ATM");
+                Console.WriteLine("3. Double Dice");
+                Console.WriteLine("Q. Leave, I guess.");
+                Console.WriteLine();
+            }
+        }
+    }
+}
